d4bed950163415e83c6eaeaeaa69fd4c7c943103
WavyCraftNetwork <142659461+Terpz710@users.noreply.github.com>
2025-03-03 21:26:18 -0800

    Update CustomForm.php

--- src/terpz710/pocketforms/CustomForm.php
+++ src/terpz710/pocketforms/CustomForm.php
@@ -12,54 +12,25 @@ class CustomForm implements Form {
 
     private string $title;
     private array $elements = [];
-    private \Closure $callback;
-
-    public function __construct(string $title, \Closure $callback) {
-        $this->setTitle($title);
-        $this->callback = $callback;
-    }
+    private ?\Closure $callback = null;
 
     public function setTitle(string $title) : self{
         $this->title = $title;
         return $this;
     }
 
-    public function addLabel(string $text) : self{
-        $this->elements[] = ["type" => "label", "text" => $text];
+    public function addElement(array $element) : self{
+        $this->elements[] = $element;
         return $this;
     }
 
-    public function addInput(string $text, string $placeholder = "", string $default = "") : self{
-        $this->elements[] = ["type" => "input", "text" => $text, "placeholder" => $placeholder, "default" => $default];
+    public function setCallback(\Closure $callback) : self{
+        $this->callback = $callback;
         return $this;
-    }
-
-    public function addDropdown(string $text, array $options, int $default = 0) : self{
-        $this->elements[] = ["type" => "dropdown", "text" => $text, "options" => $options, "default" => $default];
-        return $this;
-    }
-
-    public function addToggle(string $text, bool $default = false) : self{
-        $this->elements[] = ["type" => "toggle", "text" => $text, "default" => $default];
-        return $this;
-    }
-
-    public function addSlider(string $text, float $min, float $max, float $step = 1.0, float $default = 0) : self{
-        $this->elements[] = ["type" => "slider", "text" => $text, "min" => $min, "max" => $max, "step" => $step, "default" => $default];
-        return $this;
-    }
-
-    public function addStepSlider(string $text, array $steps, int $default = 0) : self{
-        $this->elements[] = ["type" => "step_slider", "text" => $text, "steps" => $steps, "default" => $default];
-        return $this;
-    }
-
-    public function addContent(string $text) : self{
-        return $this->addLabel($text);
     }
 
     public function handleResponse(Player $player, $data) : void{
-        if ($data !== null) {
+        if ($this->callback !== null) {
             ($this->callback)($player, $data);
         }
     }
